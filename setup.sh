befe32b766f3331b8a9ad4835e6810291a3bfe05
VedeshP <138141742+VedeshP@users.noreply.github.com>
2024-06-09 13:11:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,3 +13,10 @@ gunicorn app:app
 
 # Create a requirements file
 pip freeze > requirements.txt
+
+#If you are using GitHub Codespaces, you might need to ensure that the environment variables persist across sessions.
+#You can add environment variables to the .bashrc or .zshrc file to make them persistent:
+echo 'export SECRET_KEY=your_secret_key' >> ~/.bashrc
+
+#or for zsh:
+echo 'export SECRET_KEY=your_secret_key' >> ~/.zshrc
