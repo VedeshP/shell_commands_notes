4afc5050701d32ddb3525de5516c0ce2eb6c5a95
VedeshP <vedeshskp@gmail.com>
2025-05-31 12:36:21 +0530

    some useful commands

---
+++ battery_report.sh
@@ -0,0 +1,4 @@
+# Run the below command to get the battery report in an html file
+powercfg/batteryreport7
+
+# This command works for windows
---
+++ powershell-help.sh
@@ -0,0 +1,12 @@
+# You can use the Get-Help command to know all the details for a certain command
+
+Get-Help Start-Process -Full
+
+# Use Powershell Docs to know more about commands
+# https://learn.microsoft.com/en-us/powershell/
+
+# Search google for app specific commands 
+# Below can be used to search for chrome commands
+# chrome command line switches site:chromium.org
+# chrome arguments 
+# https://peter.sh/experiments/chromium-command-line-switches/
